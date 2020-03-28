8ee74fcf86049d7990869748b577a1c0237c38d7
Naim Mahi <naim.al.mahi@gmail.com>
2020-03-28 18:28:51 -0400

    Update CLEAN.Hs.R

--- R/CLEAN.Hs.R
+++ R/CLEAN.Hs.R
@@ -1,3 +1,3 @@
 CLEAN.Hs <-
-function() c("BioGRID", "cells", "chrLoc", "CpGislands", "ESR1targets", "geneRIFs", "GO", "GSEA", "HCL", "HUGE", "KEGG", "L2L", "MeshTable", "methylome", "modules", "MP", "reactome", "transfac","CTD","tfacts","chiptfs","tissues","rppa","mtor", "dorotheatfs","imsigs","hallmark")
+function() c("BioGRID", "cells", "chrLoc", "CpGislands", "ESR1targets", "geneRIFs", "GO", "GSEA", "HCL", "HUGE", "KEGG", "L2L", "MeshTable", "methylome", "modules", "MP", "pdbcells", "reactome", "transfac","CTD","tfacts","chiptfs","tissues","rppa","mtor", "dorotheatfs","imsigs","hallmark")
 
